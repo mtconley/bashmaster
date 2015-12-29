550083b5ce2ef1ef00ef16b0b96686c84a6d429e
Matthew Conley <mconley@teslamotors.com>
2015-12-28 21:19:44 -0800

    updated git_branch balancing

--- dotfiles/scripts/color_my_prompt.sh
+++ dotfiles/scripts/color_my_prompt.sh
@@ -9,7 +9,7 @@ color_my_prompt() {
     PWD | grep -q '/root' && text=$cwdpath_below || text=$cwdpath_above
     local __cur_location=$text
     local __git_branch_color="\[\033[1;96m\]"
-    local __git_branch="\[`git branch 2> /dev/null | grep -e ^* | sed -E  s/^\\\\\*\ \(.+\)$/\(\\\\\1\)\ /`\]"
+    local __git_branch="`git branch 2> /dev/null | grep -e ^* | sed -E  s/^\\\\\*\ \(.+\)$/\(\\\\\1\)\ /`"
     local __prompt_tail="\[\033[38;5;123m\]\\$:" # prompt color and symbol
     local __last_color="\[\033[0m\]" # command color
     export PS1="$__cur_location $__git_branch_color$__git_branch$__prompt_tail$__last_color "
