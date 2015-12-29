851c0e59b4fdc50dcb91167ef56d34b3ca960ad0
Matthew Conley <mconley@teslamotors.com>
2015-12-29 10:59:51 -0800

    add funcationality for easier file handling extension

--- dotfiles/scripts/pygments.sh
+++ dotfiles/scripts/pygments.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 # Add pygments through pip install pygments
-pygments(){
+local filetypes="xml
+           html
+           csv
+           json
+           numpy
+           bash
+           md"
+
+function pygments(){
+
+    pattern=`pattern_join $filetypes`
     if [ $# -gt 0 ]; then
-        has_lexer=`echo "$1" | grep -Eq "^xml$|^html$|^csv$|^json$|^numpy$|^bash$" && echo true || echo false`
+        has_lexer=`echo "$1" | grep -Eq $pattern && echo true || echo false`
         if [ $has_lexer == "true" ]; then
             command pygmentize -l $1 -O style="monokai" -f console256 -g
         
@@ -12,4 +22,8 @@ pygments(){
 
     fi
 
+}
+
+pattern_join(){
+    echo $@ | tr ' ' '\n' | sed 's/\(.*\)/^\1$/g' | tr '\n' '|' | rev | cut -c 2- | rev
 }
