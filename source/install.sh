c00328a6e48c65276a8e41e5f852f2ff5a2a9f54
Matthew Conley <mconley@teslamotors.com>
2015-12-29 17:48:02 -0800

    remove dots

--- source/install.sh
+++ source/install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 BASH_DIR=${PWD%/*}
 install_new_profile(){
-    line1="export BASH_DIR=$BASH_DIR"
+    line1="export BASH_DIR=$BASH_DIR/dotfiles"
     line2=". \$BASH_DIR/.bash_run"
     echo $line1 > ~/.bash_profile
     echo $line2 >> ~/.bash_profile
 }
+
+make_new_branch(){
+    name=`uname -n`
+    git checkout -b $name
+}
+
 bash $BASH_DIR/source/archive.sh
 install_new_profile
