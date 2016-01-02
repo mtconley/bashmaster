bd2a5689b1fb399cb11360f76cd40a35d60ebf47
Matthew Conley <mconley@teslamotors.com>
2016-01-02 13:45:32 -0800

    add --help opt

--- dotfiles/scripts/pyc.sh
+++ dotfiles/scripts/pyc.sh
@@ -2,10 +2,25 @@
 pyc() {
 # pipe values to python in commmand line
 # usage: ls | pyc files "files=map(lambda x: x.upper(), files.split())"
-    script="import sys
-arg=sys.argv[1]
-exec(arg + '= sys.stdin.read()')
+# Main variable '_a_r_g_' chosen to avoid collision with $1
+    case $1 in
+        -h | --help)
+            cat <<- ---
+            usage: pyc
+                pyc <object name> <python script>
+            EXAMPLES
+                ls | pyc files "files=map(lambda x: x.upper(), files.split())"
+                ls | pyc args "len(args.split())"
+            ---
+        ;;
+    esac
+    script="
+from pprint import pprint
+import sys
+_a_r_g_=sys.argv[1]
+exec(_a_r_g_ + '= sys.stdin.read()')
 exec(\"$2\")
-print eval(arg)"
-    python -c "$script" $1;
+pprint(eval(_a_r_g_))"
+    response=`python -c "$script" $1`;
+    echo -e $response
 }
