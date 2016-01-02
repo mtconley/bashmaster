43e124a57e086c3241b7de003c60bccdbd234033
Matthew Conley <mconley@teslamotors.com>
2016-01-02 02:44:15 -0800

    pyc to pass string argumetns to python interpreter from command line

---
+++ dotfiles/scripts/pyc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pyc() {
+# pipe values to python in commmand line
+# usage: ls | pyc files "print map(lambda x: x.upper(), files.split())"
+    script="import sys
+arg=sys.argv[1]
+exec(arg + '= sys.stdin.read()')
+exec(\"$2\")
+print eval(arg)"
+    python -c "$script" $1;
+}
