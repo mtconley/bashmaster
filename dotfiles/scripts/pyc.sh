9409fedd92ed0fcedcbf0ef27e2845cf29d54f81
Matthew Conley <mconley@teslamotors.com>
2016-01-02 13:58:34 -0800

    add --help

--- dotfiles/scripts/pyc.sh
+++ dotfiles/scripts/pyc.sh
@@ -5,22 +5,24 @@ pyc() {
 # Main variable '_a_r_g_' chosen to avoid collision with $1
     case $1 in
         -h | --help)
-            cat <<- ---
-            usage: pyc
-                pyc <object name> <python script>
-            EXAMPLES
-                ls | pyc files "files=map(lambda x: x.upper(), files.split())"
-                ls | pyc args "len(args.split())"
-            ---
+            help_text="
+            \r\nusage: pyc\n
+                \tpyc <object name> <python script>\n
+            EXAMPLES\n
+                \tls | pyc files \"files=map(lambda x: x.upper(), files.split())\"\n
+                \tls | pyc args \"args=len(args.split())\"\n"
+            echo -e $help_text
         ;;
-    esac
-    script="
+        *)
+            script="
 from pprint import pprint
 import sys
 _a_r_g_=sys.argv[1]
 exec(_a_r_g_ + '= sys.stdin.read()')
 exec(\"$2\")
 pprint(eval(_a_r_g_))"
-    response=`python -c "$script" $1`;
-    echo -e $response
+            response=`python -c "$script" $1`;
+            echo -e $response
+        ;;
+    esac
 }
