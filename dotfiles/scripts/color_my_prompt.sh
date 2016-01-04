eac942cf1137430246a36136de2c69f274dbf729
Matthew Conley <mconley@teslamotors.com>
2016-01-04 11:44:34 -0800

    clean up formatting

--- dotfiles/scripts/color_my_prompt.sh
+++ dotfiles/scripts/color_my_prompt.sh
@@ -7,12 +7,21 @@ color_my_prompt() {
     cwd_home="\w"
     cwdpath_below="$marker$cwd_root"
     cwdpath_above="$cwd_home"
-    PWD | grep -q '/root' && text=$cwdpath_below || text=$cwdpath_above
-    local __cur_location="\[\033[38;5;229m\]$text"
-    local __git_branch_color="\[\033[1;96m\]"
+    PWD | grep -q '/root' && loc_text=$cwdpath_below || loc_text=$cwdpath_above
+
+    local __cur_location_color="\[\033[38;5;229m\]"
+    local __cur_location_text="$loc_text"
+    local __cur_location=$__cur_location_color$__cur_location_text
+
     # __git_branch must have command wrapped in single quotes to update on checkout...
-    local __git_branch='`git branch 2> /dev/null | grep -e ^* | sed -E  s/^\\\\\*\ \(.+\)$/\(\\\\\1\)\ /`'
-    local __prompt_tail="\[\033[38;5;123m\]\\$:" # prompt color and symbol
+    local __git_branch_color="\[\033[1;96m\]"
+    local __git_branch_text='`git branch 2> /dev/null | grep -e ^* | sed -E  s/^\\\\\*\ \(.+\)$/\(\\\\\1\)\ /`'
+    local __git_branch=$__cur_branch_color$__cur_branch_text
+
+    local __prompt_tail_color="\[\033[38;5;123m\]"
+    local __prompt_tail_text="\\$:" # prompt color and symbol
+    local __prompt_tail=$__prompt_tail_color$__prompt_tail_text
+
     local __last_color="\[\033[0m\]" # command color
-    export PS1="$__cur_location $__git_branch_color$__git_branch$__prompt_tail$__last_color "
+    export PS1="$__cur_location $__git_branch$__prompt_tail$__last_color "
 }
