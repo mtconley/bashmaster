f3c2dcca1ed4638bee2dee24d83f003574a2885d
Matthew Conley <mconley@teslamotors.com>
2016-01-19 15:05:55 -0800

    remove cruft from laster merge master

--- dotfiles/scripts/color_my_prompt.sh
+++ dotfiles/scripts/color_my_prompt.sh
@@ -25,16 +25,4 @@ color_my_prompt() {
 
     local __last_color="\[\033[0m\]" # command color
     export PS1="$__cur_location $__git_branch$__prompt_tail$__last_color "
-=======
-color_my_prompt ()
-{
-    local __cur_locn_c="\[\033[01;34m\]";
-    local __cur_locn="\w";
-    local __git_branch_c="\[\033[35m\]";
-    local __git_branch='`git branch 2> /dev/null | grep -e ^* | sed -E  s/^\\\\\*\ \(.+\)$/\(\\\\\1\)\ /`';
-    local __prompt_tail_c="\[\033[36m\]";
-    local __prompt_tail="⚙";          # or any other cool unicode symbol i.e. ><(((º> 
-    local __input_c="\[\033[00m\]";
-    export PS1="$__cur_locn_c$__cur_locn $__git_branch_c$__git_branch$__prompt_tail_c$__prompt_tail$__input_c "
->>>>>>> 250621149edd6ee8cb4e1fff0135727514102f87
 }
